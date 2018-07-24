67ddc5ed4eb0ae66659f57fc42ad6aed8f5b7374
Franck2509 <franck97@bluewin.ch>
2018-07-24 17:14:34 +0200

    form and average created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  score: number;
+  total: number;
+  amount: number;
+
+  value() {
+    this.total = 0;
+    this.amount = 0;
+    if (((<HTMLInputElement>document.getElementById('valueq1')).value) !== 'null') {
+      this.total += parseInt((<HTMLInputElement>document.getElementById('valueq1')).value);
+      this.amount += 1;
+    }
+
+    if (((<HTMLInputElement>document.getElementById('valueq2')).value) !== 'null') {
+      this.total += parseInt((<HTMLInputElement>document.getElementById('valueq2')).value);
+      this.amount += 1;
+    }
+
+    if (((<HTMLInputElement>document.getElementById('valueq3')).value) !== 'null') {
+      this.total += parseInt((<HTMLInputElement>document.getElementById('valueq3')).value);
+      this.amount += 1;
+    }
+    this.score = this.total / this.amount;
+  }
 }
